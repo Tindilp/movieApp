a33af0a9d3eec48f379b53588248bb28d637b132
Lucas Di Lorenzo <lucasdilorenzo_89@outlook.com>
2021-12-12 10:56:06 -0300

    - added screens - added alamofire

---
+++ movieApp/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  movieApp
+//
+//  Created by Lucas Di Lorenzo on 12/12/2021.
+//
+
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func cancelBtnPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
