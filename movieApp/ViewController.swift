a8312b0448d357ed1db66241b21f019d736bdf8a
Lucas Di Lorenzo <lucasdilorenzo_89@outlook.com>
2021-12-12 09:47:49 -0300

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Lucas Di Lorenzo on 12/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
